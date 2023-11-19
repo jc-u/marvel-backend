6ec6e58f4ec93012df18a3d7ed022e227ef686e7
jc-u <jcurbain.admin@gmail.com>
2023-11-19 18:06:30 +0100

    previous version

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI);
-
 const serverRoutes = require("./routes/server");
-const userRoutes = require("./routes/user");
 
 app.use(serverRoutes);
-app.use(userRoutes);
 
 app.all("*", (req, res) => {
 	res.status(404).json({ message: "Not Found" });
